36bd86e015b53165ba7b35f40eb68cae3698e8ed
Eduardo Hernandez <edua.ahc@gmail.com>
2023-03-01 19:46:57 -0600

    Se avanza en la tabla de programacion de entrega

---
+++ CEMET/SP/Table_Programacion_Recoleccion.sql
@@ -0,0 +1,11 @@
+CREATE TABLE dbo.Programacion_Recoleccion_Solicitud(
+	PROG_Id int PRIMARY KEY IDENTITY (1, 1),
+	PROG_NO_Programacion int NOT NULL,
+	PROG_Fecha_Recoleccion DATETIME NOT NULL,
+	PROG_Contacto varchar(35) NOT NULL,
+	PROG_Telefono varchar(15) NOT NULL,
+	PROG_Direccion varchar(95) NOT NULL,
+	PROG_Cantidad_Bultos int NOT NULL,
+	PROG_Cantidad_Solicitudes int NOT NULL,
+	PROG_Referencias_Adicionales varchar(255) NULL
+)
